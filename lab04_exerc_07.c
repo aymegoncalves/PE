a6d4e64d9d97f016e0d4e91fef5770c5b0ad7f81
Aymê Cardoso Gonçalves <107072147+aymegoncalves@users.noreply.github.com>
2023-11-08 12:09:12 -0300

    lab04_exerc_07.c

---
+++ lab04_exerc_07.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void strcat(char a, char b){
+  printf("%s%s", a, b);
+}
+int main(){
+  char a, b;
+  printf("Digite uma string: ");
+  fgets(a, stdin);
+  getchar();
+  printf("Digite uma string: ");
+  fgets(b, stdin);
+
+  strcat(a,b);
+}
