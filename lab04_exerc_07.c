39e0d07b49e348cc65323e517fa92045b2f84a1d
Aymê Cardoso Gonçalves <107072147+aymegoncalves@users.noreply.github.com>
2023-11-09 10:08:08 -0300

    Update lab04_exerc_07.c

--- lab04_exerc_07.c
+++ lab04_exerc_07.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-void strcat(char a, char b){
-  printf("%s%s", a, b);
+void strcat(char* a, char* b){
+  printf("%s", a);
+  printf("%s\n", b);
 }
 int main(){
-  char a, b;
+  char* a = malloc(10 * sizeof(char));
+  char* b = malloc(10 * sizeof(char));
   printf("Digite uma string: ");
-  fgets(a, stdin);
+  fgets(a, sizeof(a), stdin);
   getchar();
   printf("Digite uma string: ");
-  fgets(b, stdin);
+  fgets(b, sizeof(b), stdin);
 
   strcat(a,b);
+
+  free(a);
+  free(b);
+  return 0;
 }
